cc37494f1b02ec8b5a13f3fbb066d69173a804de
theo-mc <tmcaleer2k18@gmail.com>
2024-07-13 21:25:20 +0100

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+
+local opt = vim.opt
+
+-- opt.laststatus = 2
+-- opt.cmdheight = 0
+-- vim.g.VM_set_statusline = 0
+-- vim.g.VM_silent_exit = 1
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "Shatur/neovim-ayu" },
+  -- { "rebelot/kanagawa.nvim" },
+  -- { "olimorris/onedarkpro.nvim" },
+  -- { "projekt0n/github-nvim-theme" },
+  -- { "sainnhe/everforest" },
+  -- { "marko-cerovac/material.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  -- { "catppuccin/nvim" },
+  -- { "EdenEast/nightfox.nvim" },
+  -- { "rose-pine/neovim" },
+  -- { "scottmckendry/cyberdream.nvim" },
+  -- { "tiagovla/tokyodark.nvim" },
+  { "" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu",
+    },
+  }
+}
