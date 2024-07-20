eb35610bbe372f1d34ac4b86b2161fa56b4241e0
theo-mc <tmcaleer2k18@gmail.com>
2024-07-20 19:09:57 +0100

    last commit again

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,13 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-        options = {
-            disabled_filetypes = { "dashboard" },
-            globalstatus = true,
-            section_separators = { left = '', right = '' },
-            component_separators = { left = '', right = '' }
-        },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                disabled_filetypes = { "dashboard" },
+                globalstatus = true,
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' }
+            },
+        }
     }
 }
