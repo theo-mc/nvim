3d342bfa5e61b82577c373815683b3c608016ccd
theo-mc <tmcaleer2k18@gmail.com>
2024-09-22 22:31:29 +0100

    updated base removed unnecessary plugin options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
 return {
-    -- { "ellisonleao/gruvbox.nvim" },
-    { "Shatur/neovim-ayu" },
-    -- { "rebelot/kanagawa.nvim" },
-    -- { "olimorris/onedarkpro.nvim" },
-    -- { "projekt0n/github-nvim-theme" },
-    -- { "sainnhe/everforest" },
-    -- { "marko-cerovac/material.nvim" },
-    -- { "nyoom-engineering/oxocarbon.nvim" },
-    { "AlexvZyl/nordic.nvim" },
-    -- { "catppuccin/nvim" },
-    -- { "EdenEast/nightfox.nvim" },
-    -- { "rose-pine/neovim" },
-    -- { "scottmckendry/cyberdream.nvim" },
-    -- { "tiagovla/tokyodark.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "Shatur/neovim-ayu" },
+  -- { "rebelot/kanagawa.nvim" },
+  -- { "olimorris/onedarkpro.nvim" },
+  -- { "projekt0n/github-nvim-theme" },
+  -- { "sainnhe/everforest" },
+  -- { "marko-cerovac/material.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  -- { "catppuccin/nvim" },
+  -- { "EdenEast/nightfox.nvim" },
+  -- { "rose-pine/neovim" },
+  -- { "scottmckendry/cyberdream.nvim" },
+  -- { "tiagovla/tokyodark.nvim" },
 
-    -- Configure LazyVim to load theme    {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "ayu",
-        },
-    }
+  -- Configure LazyVim to load theme    {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu",
+    },
+  },
 }
